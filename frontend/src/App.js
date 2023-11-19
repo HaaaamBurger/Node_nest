c6906cda1e0e1e219bc146bc8de8e5cd65de8619
HaaaamBurger <gemasclashes@gmail.com>
2023-11-19 15:01:04 +0200

    #

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/users")
+      .then(({data}) => setUsers(data))
+  }, []);
+
+  console.log(users);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        users.map(user => <div>{user.email}</div>)
+      }
     </div>
   );
-}
+};
 
 export default App;
